eccbc872f29373a710d13f37d4c6235eea83d4c8
Kei Nishina <k247loveke@gmail.com>
2015-10-06 17:56:53 +0859

    tune for the change of K247_qgcm.rb

--- analyze_data.rb
+++ analyze_data.rb
@@ -2,13 +2,6 @@ require "~/lib_k247/K247_qgcm"
 
 watcher = K247_Main_Watch.new
 
-if ARGV[0] != nil
-  qgd = K247_qgcm_data.new( ARGV[0] )
-  #qgd.chk_energy_avg_ncout
-  # test methods  ( temporary@ 2015-10-06 )
-  #K247_qgcm_data.prep_set_greater_cname
-else
-  exit_with_msg("input case name")
-end
+qgd = K247_qgcm_data.new( ARGV[0] )
 
 watcher.end_process
