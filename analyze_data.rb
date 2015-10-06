d8bd79e95a7df7afd50367ae0e42ee9fc4a43088
Kei Nishina <k247loveke@gmail.com>
2015-10-06 16:08:31 +0859

    coffee break

--- analyze_data.rb
+++ analyze_data.rb
@@ -4,7 +4,7 @@ watcher = K247_Main_Watch.new
 
 if ARGV[0] != nil
   qgd = K247_qgcm_data.new( ARGV[0] )
-  qgd.chk_energy_avg_ncout
+  #qgd.chk_energy_avg_ncout
   # test methods  ( temporary@ 2015-10-06 )
   #K247_qgcm_data.prep_set_greater_cname
 else
