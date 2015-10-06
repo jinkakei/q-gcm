54ff5c0d0ffe349eae115c1ab79f30e256efe84a
Kei Nishina <k247loveke@gmail.com>
2015-10-07 07:51:25 +0859

    change unite with library update

--- unite_outdata.rb
+++ unite_outdata.rb
@@ -1,9 +1,5 @@
 require "~/lib_k247/K247_qgcm"
 
-if ARGV[0] != nil
-  K247_qgcm_data.prep_integrate_outdata( ARGV[0] )
-  # test methods prep_* ( temporary@ 2015-10-06 )
-  #K247_qgcm_data.prep_set_greater_cname
-else
-  exit_with_msg("input case name")
-end
+
+K247_qgcm_data.prep_integrate_outdata( ARGV[0] )
+
