b0464fec8aa655dd8ff29c0a75d2a8499d114d29
Kei Nishina <k247loveke@gmail.com>
2015-10-12 11:40:26 +0900

    small test for analyze_data.rb

--- analyze_data.rb
+++ analyze_data.rb
@@ -1,7 +1,13 @@
-require "~/lib_k247/K247_qgcm"
+require_relative "qgcm_k247"
+#require_relative "lib_k247_for_qgcm"
+#require "~/lib_k247/K247_qgcm"
 
-watcher = K247_Main_Watch.new
+#watcher = K247_Main_Watch.new
 
-qgd = K247_qgcm_data.new( ARGV[0] )
+#qgd = K247_qgcm_data.new( ARGV[0] )
+qgd = K247_qgcm_data.new( "dx4km2y" ) # test@2015-10-12
 
-watcher.end_process
+# check energy
+  qgd.chk_energy_avg_stdout
+
+#watcher.end_process
