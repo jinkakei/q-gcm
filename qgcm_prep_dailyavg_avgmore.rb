bef98db652227c5550dc85e3db8c8500122c372f
Kei Nishina <k247loveke@gmail.com>
2015-11-05 08:16:44 +0900

    add preprocessor of dailyavg data

---
+++ qgcm_prep_dailyavg_avgmore.rb
@@ -0,0 +1,75 @@
+require_relative 'qgcm_k247'
+require_relative "varray_proto_k247"
+
+#va = VArray_Proto_K247.new( \
+#       NArray.sfloat(10).indgen, {"units"=>"km"}, "xaxis")
+
+watcher = K247_Main_Watch.new
+
+#outdir = "./outdata_nctest47/" # daily average: 4km, 481x481, 20day
+outdir = "./outdata_nctest48/" # daily average: 4km, 481x481, 730day
+orgdir = "avg/"
+flist  = Dir::glob( outdir + orgdir + "*.nc").sort
+#  p flist[0]
+
+#nday = flist.length
+nday = 30
+
+# initial set ( @nxp, @nyp, @nz: qgcm_k247.rb )
+  gp_p = GPhys::IO.open( flist[0], "p" )
+  ptmp = gp_p.val
+  nl = ptmp.shape
+  psum_pre  = NArray.sfloat( nl[0], nl[1], nl[2] ).fill( 0.0 )
+  psum_post = NArray.sfloat( nl[0], nl[1], nl[2] ).fill( 0.0 )
+# prepare
+  avgday = 10 
+  avgdir = "avg#{avgday}day/"
+  exec_command( "mkdir #{outdir + avgdir}" )
+  new_grid = K247_qgcm_data.prep_modify_po_grid_tmp( gp_p )
+  pavg_attr = {"units"=>"m2.s-2", \
+               "long_name"=>"Ocean Dynamic Pressure", \
+               "avg_period"=>"#{2*avgday}"}
+  vap_pavg = VArray_Proto_K247.new( nil, pavg_attr, "pavg", new_grid )
+for t in 1..avgday
+  psum_pre  += GPhys::IO.open( flist[t-1], "p" ).val
+end
+for t in avgday+1..nday
+  psum_post += GPhys::IO.open( flist[t-1], "p" ).val
+  if ( t.modulo( avgday ) == 0 )
+    puts "outdata: #{t}"
+    puts "  #{flist[t-1]}"
+    str_day = sprintf( "%06d", t - avgday )
+    fname = "#{outdir+avgdir}poavg_#{str_day}day.nc"
+    pavg = ( psum_pre + psum_post ) / ( 2 * avgday ).to_f
+    vap_pavg.chg_nary( pavg )
+    vap_pavg.netcdf_write_create( fname )
+    psum_pre  = psum_post.clone
+    psum_post = 0.0
+  end
+end
+# postprocess
+# [!] ToDo: Use ocpo.nc
+  na_p0 = GPhys::IO.open( "#{outdir}ocpo.nc", "p" ).cut( "time"=> 0.0 ).val
+  vap_pavg.chg_nary( na_p0 )
+  avgday0 = sprintf( "%06d", 0 )
+  fname0 = "#{outdir+avgdir}poavg_#{avgday0}day.nc"
+  vap_pavg.netcdf_write_create( fname0 )
+
+=begin
+  # distorted ?
+  avgday1 = sprintf( "%06d",  avgday )
+  fname1 = "#{outdir+avgdir}poavg_#{avgday1}day.nc"
+  na_p1 = GPhys::IO.open( fname1, "pavg" ).val
+  avgday2 = sprintf( "%06d",  2 * avgday )
+  fname2 = "#{outdir+avgdir}poavg_#{avgday2}day.nc"
+  na_p2 = GPhys::IO.open( fname2, "pavg" ).val
+  na_dp = ( na_p2 - na_p1 )
+  na_p0 = na_p1 - na_dp
+  avgday0 = sprintf( "%06d", 0 )
+  fname0 = "#{outdir+avgdir}poavg_#{avgday0}day.nc"
+  vap_pavg.chg_nary( na_p0 )
+  vap_pavg.netcdf_write_create( fname0 )
+=end
+
+
+watcher.end_process
---
+++ qgcm_prep_dailyavg_hmax.rb
@@ -0,0 +1,99 @@
+require_relative 'qgcm_k247'
+require_relative "varray_proto_k247"
+# ToDo:
+#    
+#   - What is required? 
+#    
+#   - enhance methods and refactoring
+#   - test 2day average ( aliasing? )
+#   - 
+
+def set_grid_day( nday )
+  att_day = {"units"=>"days","long_name"=>"Time Axis"}
+  na_day   = NArray.sfloat(nday).indgen + 0.5
+  vap_day  = VArray_Proto_K247.new( na_day, att_day, "time")
+  return vap_day.get_grid
+end # def set_grid_day
+
+def sshmax_get_with_ij_from_fname( fname )
+# fname -- ./outdata_*/avg/ocavg_??????day.nc 
+  # ToDo: check fname and set mode
+# get 2d NArray 
+  gp_p = GPhys::IO.open( fname, "p" )
+    na_z = gp_p.coord("z").val
+  na_p0 = gp_p.cut( "z"=>na_z[0]).val
+# 
+  pmax, i, j = na_max_with_index_k247( na_p0 )
+  m_to_cm = 100.0; grav = 9.8
+  hmax = pmax * m_to_cm / grav
+  return hmax, i, j
+end
+watcher = K247_Main_Watch.new
+
+#outdir = "./outdata_nctest44/" # raw: 2km, 481x481, 730day
+#outdir = "./outdata_nctest46/" # raw: 4km, 481x481, 20day
+#outdir = "./outdata_nctest47/" # daily average: 4km, 481x481, 20day
+outdir = "./basic/" # daily average: 2km, 3841x1921, 730day
+avgdir = "avg/"
+flist  = Dir::glob( outdir + avgdir + "*.nc").sort
+#  p flist[0]
+
+
+nc_fu = NetCDF.create( "#{outdir}hmax_etc.nc" )
+  nday = flist.length
+  #nday = 10
+  # set day grid
+    grid_day = set_grid_day( nday )
+      na_day = grid_day.coord(0).val # (kari)
+  # set hmax
+  hmax = NArray.sfloat( nday )
+    hmax_i = NArray.int( nday )
+    hmax_j = NArray.int( nday )
+  for n in 0..nday-1
+  #  hmax[n] = GPhys::IO.open( flist[n], "p" ).val.max / 9.8 * 100.0
+    hmax[n], hmax_i[n], hmax_j[n] \
+      = sshmax_get_with_ij_from_fname( flist[n])
+  end
+    attr_hmax = {"units"=>"cm","long_name"=>"SSH Max"}
+    vap_hmax  = VArray_Proto_K247.new( hmax, attr_hmax, "hmax", grid_day )
+    vap_hmax.netcdf_write( nc_fu )
+    attr_hmax_i = {"units"=>"","long_name"=>"SSH Max X Position"}
+    vap_hmax_i  = VArray_Proto_K247.new( hmax_i, attr_hmax_i, "hmax_i", grid_day )
+    vap_hmax_i.netcdf_write( nc_fu )
+    attr_hmax_j = {"units"=>"","long_name"=>"SSH Max Y Position"}
+    vap_hmax_j  = VArray_Proto_K247.new( hmax_j, attr_hmax_j, "hmax_j", grid_day )
+    vap_hmax_j.netcdf_write( nc_fu )
+  # set hdec
+  hdec = NArray.sfloat( nday )
+    xspd = NArray.sfloat( nday )
+    yspd = NArray.sfloat( nday )
+      dxo = 2.0e5 # [cm], tmp
+      day_to_sec = 24.0 * 3600.0
+  for n in 1..nday-2
+    hdec[n] = ( hmax[n+1] - hmax[n-1] ) / \
+               ( na_day[n+1] - na_day[n-1] ) * 365.0
+    #xspd[n] = dxo * ( hmax_i[n+1] - hmax_i[n-1] ) / \
+    xspd[n] = dxo * ( hmax_i[n+1] - hmax_i[n-1] ).to_f / \
+                ( ( na_day[n+1] - na_day[n-1] ) * day_to_sec )
+    #yspd[n] = dxo * ( hmax_j[n+1] - hmax_j[n-1] ) / \
+    yspd[n] = dxo * ( hmax_j[n+1] - hmax_j[n-1] ).to_f / \
+                ( ( na_day[n+1] - na_day[n-1] ) * day_to_sec )
+  end
+    hdec[0] = hdec[1]; hdec[nday-1] = hdec[nday-2]
+    attr_hdec = {"units"=>"cm/year","long_name"=>"SSH Max Decay Rate"}
+    vap_hdec  = VArray_Proto_K247.new( hdec, attr_hdec, "hdec", grid_day )
+    vap_hdec.netcdf_write( nc_fu )
+    xspd[0] = xspd[1]; xspd[nday-1] = xspd[nday-2]
+    attr_xspd = {"units"=>"cm/s", \
+                 "long_name"=>"Zonal Translation Speed of SSH Max"}
+    vap_xspd  = VArray_Proto_K247.new( xspd, attr_xspd, "xspd", grid_day )
+    vap_xspd.netcdf_write( nc_fu )
+    yspd[0] = yspd[1]; yspd[nday-1] = yspd[nday-2]
+    attr_yspd = {"units"=>"cm/s", \
+                 "long_name"=>"Meridional Translation Speed of SSH Max"}
+    vap_yspd  = VArray_Proto_K247.new( yspd, attr_yspd, "yspd", grid_day )
+    vap_yspd.netcdf_write( nc_fu )
+
+nc_fu.close
+
+watcher.end_process
