928b029139803a42cbaef4cb7279b681538b39d3
Kei Nishina <k247loveke@gmail.com>
2015-11-04 20:58:55 +0900

    unite_outdata.rb: update for qgcm_prep_k247.rb

--- unite_outdata.rb
+++ unite_outdata.rb
@@ -1,5 +1,10 @@
-require_relative "qgcm_k247.rb"
+require_relative "qgcm_prep_k247"
 
-
-K247_qgcm_data.prep_unify_outdata( ARGV[0] )
-
+cname = ARGV[0]
+qg_prep = k247_qgcm_preprocess_wrapper( cname )
+  unless qg_prep.orgfile_exist?
+    puts "!Error! original file is not exist!"
+    exit
+  end
+  qg_prep.para_ncout
+  qg_prep.monit_ncout
